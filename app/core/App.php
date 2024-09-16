1be5b3feb21c60cb0a7134f44ce32dc5f195438d
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-16 10:48:05 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,17 @@
 
 class App {
 	public function __construct () {
-		echo 'APP OK' . '<br>';
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL() {
+		if (isset($_GET['url'])) {
+			$url = $_GET['url'];
+			$url = rtrim($url, '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
 	}
 }
