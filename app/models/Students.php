4a662a775e8928b01ebcc03a9ca82d7271064309
habibullah <ibnumalikalbustomi@gmail.com>
2024-09-16 23:07:34 +0700

    model contains arrays obtained from database

--- app/models/Students.php
+++ app/models/Students.php
@@ -1,34 +1,27 @@
 <?php
 
 class Students {
-	private $students = [
-		[
-			'name' => 'Sandhika Galih',
-			'nrp' => '043040023',
-			'email' => 'sandhikagalih@unpas.ac.id',
-			'major' => 'Informatics Engineering'
-		],
-		[
-			'name' => 'Doddy Ferdiansyah',
-			'nrp' => '133050321',
-			'email' => 'doddy@gmail.com',
-			'major' => 'Mechanical Engineering'
-		],
-		[
-			'name' => 'Erik Doank',
-			'nrp' => '163030123',
-			'email' => 'erik@yahoo.com',
-			'major' => 'Industrial Engineering'
-		],
-		[
-			'name' => 'Fahrur Rozy',
-			'nrp' => '696956969',
-			'email' => 'takendekrogi@krembun.com',
-			'major' => 'Economic Engineering'
-		]
-	];
+	private $dbh;
+	private $stmt;
+
+	public function __construct() {
+		$dbHost = 'localhost';
+        $dbName = 'local_testing';
+        $dbUser = 'root';
+        $dbPass = '';
+		$dsn = "mysql:host=$dbHost;dbname=$dbName";
+		try {
+			$this->dbh = new PDO($dsn, $dbUser, $dbPass);
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+	}
 
 	public function getAllStudents() {
-		return $this->students;
+		$sql = 'SELECT * FROM testing_student';
+		$this->stmt = $this->dbh->prepare($sql);
+		$this->stmt->execute();
+		$students = $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		return $students;
 	}
 }
